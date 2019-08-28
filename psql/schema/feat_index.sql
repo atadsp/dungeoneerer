3fc52ea631e5d02f2899ed38b354f37ea13317c8
atadsp <atadsp@gmail.com>
2019-08-28 16:12:29 -0400

    moved some stuff

--- psql/schema/feat_index.sql
+++ psql/schema/feat_index.sql
@@ -6,11 +6,11 @@ CREATE TABLE dungeoneerer.feat_index
 (
     feat_index_id SERIAL PRIMARY KEY,
     feat_id INT NOT NULL,
+    feat_name_id INT NOT NULL,
     version_id INT NOT NULL,
     book_id INT NOT NULL,
-    feat_name_id INT NOT NULL,
     FOREIGN KEY (feat_id) REFERENCES dungeoneerer.feats(feat_id),
+    FOREIGN KEY (feat_name_id) REFERENCES dungeoneerer.feat_names(feat_name_id),
     FOREIGN KEY (version_id) REFERENCES dungeoneerer.versions(version_id),
-    FOREIGN KEY (book_id) REFERENCES dungeoneerer.books(book_id),
-    FOREIGN KEY (feat_name_id) REFERENCES dungeoneerer.feat_names(feat_name_id)
+    FOREIGN KEY (book_id) REFERENCES dungeoneerer.books(book_id)
 );
