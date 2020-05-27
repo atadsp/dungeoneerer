170bc1c3d8288fc27c0edc5591f50391ecc1a3be
atadsp <atadsp@gmail.com>
2020-05-26 22:52:21 -0400

    fix an issue

--- server/routes/front-router.handler.ts
+++ server/routes/front-router.handler.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import * as express from "express";
 import expressVue from "express-vue";
 
 class FrontEndRouter {
     public build(app: express.Express): express.Express {
 
         expressVue.use(app).then(() => {
-            app.get("/", (req, res) => {
+            app.get("/", (req: any, res: any) => {
 
                 const data = {
                     title: "TEST",
